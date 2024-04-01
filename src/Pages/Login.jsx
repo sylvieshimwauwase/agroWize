3fbe1cd8ddc1cc6b84adb0bef7229dfc5d04026b
sylvieshimwauwase <slvshimwa@gmail.com>
2024-04-01 10:36:05 +0200

    adding updates on login header

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import FormNavbar from '../Components/FormNavbar/FormNavbar.jsx';
 import LoginForm from '../Components/LoginForm/LoginForm.jsx';
-import HeaderBar from '../Components/Header/HeaderBar.jsx';
-
 
 const Login = () => {
   return (
