6cf4847601171c9c19d07a14b68785a147d67939
sylvieshimwauwase <slvshimwa@gmail.com>
2024-04-13 22:17:12 +0200

    adding Aboutus page

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './AboutUs.css';
+import CoreValues from "../CoreValues/CoreValues";
+import Footer from "../Footer/Footer";
+import Team from "../Team/Team";
 
 const HeroSection = () => {
     return (
@@ -13,9 +16,13 @@ const HeroSection = () => {
                 </div>
                 <div className="row-section-item">
                     <p>Welcome to <span style={{ color: "#226d2c" }}>AgroWize</span>, the foremost source of
+                        <br />
                         agricultural knowledge in Africa. Our goal is to
+                        <br />
                         lead the way in transforming the farming
+                        <br />
                         sector by providing comprehensive education
+                        <br />
                         and training on sustainable agricultural practices.
                     </p>
                     <img className="sectionImage2" src="/AboutUsPicture 1.png" alt="Logo" />
@@ -23,25 +30,41 @@ const HeroSection = () => {
                 <div className="row-section-item">
                     <img className="sectionImage2" src="/AboutUs4.png" alt="Logo" />
                     <p>We are committed to promoting sustainable
+                    <br />
                         agricultural practices and our mission is to
+                        <br />
                         contribute to the restoration and maintenance
+                        <br />
                         of ecosystem balance. We aim to achieve this
+                        <br />
                         by engaging with our community, promoting
+                        <br />
                         communication, knowledge sharing, and
+                        <br />
                         collaboration. Our goal is to drive positive
+                        <br />
                         change in the agricultural sector throughout
+                        <br />
                         Africa by fostering innovation.
                     </p>
                 </div>
                 <div className="row-section-item">
-                    <p>At <span style={{ color: "#f29620" }}>AgroWize</span>, we are committed to advancing
+                    <p>At <span style={{ color: "#f29620" }}>AgroWize</span>, we are committed to advancing 
+                    <br />
                         agricultural knowledge and promoting the
+                        <br />
                         adoption of sustainable practices among
+                        <br />
                         farmers throughout Africa. Our community
+                        <br />
                         platform aims to foster a culture of
+                        <br />
                         collaboration and knowledge exchange that
+                        <br />
                         empowers farmers to make informed decisions
+                        <br />
                         and drive positive change in the agricultural
+                        <br />
                         industry.
                     </p>
                     <img className="sectionImage2" src="/AboutUsPicture 2.png" alt="Logo" />
@@ -49,54 +72,16 @@ const HeroSection = () => {
                 <div className="row-section-item">
                     <img className="sectionImage2" src="/AboutUs3.png" alt="Logo" />
                     <p>We invite you to join us on this journey
+                    <br />
                         towards a more sustainable and thriving
+                        <br />
                         agricultural sector in Africa. <span style={{ color: "#f29620"}}>By working
+                        <br />
                         together, we can make a significant difference.</span></p>
                 </div>
-                <div className="missionVisionValuesDiv">
-                    <div className="innerDiv">
-                        <img src="image1.jpg" alt="Image 1" />
-                        <h3>Mission</h3>
-                        <p>AgroWize aims to foster the 
-                            advancement of agricultural knowledge 
-                            and the adoption of sustainable eco-
-                            friendly practices among farmers 
-                            throughout Africa through agricultural 
-                            education and collaboration.
-                        </p>
-                    </div>
-                    <div className="innerDiv">
-                        <img src="image2.jpg" alt="Image 2" />
-                        <h3>Vision</h3>
-                        <p>AgroWize aims to be the foremost 
-                            resource for agricultural knowledge in 
-                            Africa, driving a paradigm shift in the 
-                            farming sector through farmers' 
-                            education and training in sustainable 
-                            agricultural practices.
-                        </p>
-                    </div>
-                    <div className="innerDiv">
-                        <img src="image3.jpg" alt="Image 3" />
-                        <h3>Core Values</h3>
-                        <p><span style={{color:"#226d2c"}}>Innovation:</span>Innovation: Innovation is woven into the 
-                            fabric of all our solutions as an Agrotech 
-                            company.
-                        </p>
-                        <p><span style={{color:"#f29620"}}>Leadership:</span> We strive to be the leaders 
-                            in revolutionizing Agriculture.
-                        </p>
-                        <p><span style={{color:"#2b2222"}}>Leadership:</span> Creating impact through 
-                            sustainable farming practices.
-                        </p>
-                    </div>
-                </div>
-                <div className="section footer-section">
-                    <footer>
-                        <p>Contact us: contact@example.com</p>
-                        <p>Address: 123 Street, City, Country</p>
-                    </footer>
-                </div>
+                <CoreValues />
+                <Team />
+                <Footer />
             </div>
     );
 };
---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import './Team.css';
+
+const Team = () => {
+      return (
+            <div>
+                  <section className="teamSection">
+                        <div className="teamTitle">
+                              <h3>Meet Our Team Members</h3>
+                        </div>
+                <div className="teamGrid">
+                    <div className="teamMember">
+                        <img src="/Mentor.png" alt="Mentor" />
+                        <p>Emmanuel Daniel</p>
+                        <p>Mentor</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Juliana A..png" alt="Team Member" />
+                        <p>Juliana Akpaette</p>
+                        <p>Product Manager</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Juliana A-Martins.png" alt="Team Member" />
+                        <p>Juliana Adeniyi-Martins</p>
+                        <p>Product Manager</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Justina Olayemi.png" alt="Team Member" />
+                        <p>Justina Olayemi</p>
+                        <p>Product Manager</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Kidane.png" alt="Team Member" />
+                        <p>Kidstemariam Kidane</p>
+                        <p>Product Manager</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Toyibat Salako.png" alt="Team Member" />
+                        <p>Toyibat Salako</p>
+                        <p>Product Designer</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Tracy O..png" alt="Team Member" />
+                        <p>Tracy Omoike</p>
+                        <p>Data Analyst</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Uchenna O..png" alt="Team Member" />
+                        <p>Uchenna Okafor</p>
+                        <p>Data Analyst</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Sylvie's Selfie.jpeg" alt="Team Member" />
+                        <p>Sylvie SHIMWA UWASE</p>
+                        <p>Front-End Developer</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Temilola.png" alt="Team Member" />
+                        <p>Temilola Oguntamu</p>
+                        <p>Front-End Developer</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Roberta O..png" alt="Team Member" />
+                        <p>Roberta Okorie</p>
+                        <p>Cyber Security</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Tolulope A..png" alt="Team Member" />
+                        <p>Tolulope Adesanya</p>
+                        <p>Back-End Developer</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Faith M..png" alt="Team Member" />
+                        <p>Faith Madamedon</p>
+                        <p>Data Analyst</p>
+                    </div>
+                    <div className="teamMember">
+                        <img src="/Oluwadamilola.png" alt="Team Member" />
+                        <p>Oluwadamilola Ogunmodede</p>
+                        <p>Product Designer</p>
+                    </div>
+                        </div>
+                  </section>
+            </div>
+      );
+};
+
+export default Team;
