786b5630c06bda0eaec91ab9158f018203c30847
Dhoradex <temilolaoguntamu@outlook.com>
2024-04-16 20:27:38 +0100

    added continue learning onclick

--- src/Components/Resources/Resources.jsx
+++ src/Components/Resources/Resources.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Resources.css"
 import { products } from '../../Constants/Products';
 import FormButton from '../FormButton';
 import SearchBar from '../Search/SearchBar';
 
 
-
 const Resources = () => {
+    const [learningStates, setLearningStates] = useState(
+      Array(products.length).fill(false)
+    );
+
+    const handleLearningClick = (index) => {
+      const newLearningStates = [...learningStates];
+      newLearningStates[index] = true;
+      setLearningStates(newLearningStates);
+    };
+
   return (
     <>
       <section>
@@ -16,16 +25,21 @@ const Resources = () => {
         <img className="images" src="/VerticalLearning.png" alt="Image" />
         <SearchBar/>
       </div>
-      {products.map((item) => {
+      {products.map((item, index) => {
         return (
-          <div className="productDetails">
+          <div className="productDetails" key={index}>
             <img className="images" src={item.image} alt="Image" />
             <div className="borderDetails">
               <h3>{item.title}</h3>
               <h6>Duration: 1 - 3 Months</h6>
               <h6>Certificate: Available Upon Completion</h6>
               <h5 className="primaryColor">Online</h5>
-              <FormButton name="Start Learning" />
+              <FormButton
+                name={
+                  learningStates[index] ? "Continue Learning" : "Start Learning"
+                }
+                onClick={() => handleLearningClick(index)}
+              />
             </div>
           </div>
         );
